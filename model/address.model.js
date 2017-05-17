f0c1a53c9a327ba51d301dcdc079b1b9bf498693
Ian Hamilton <ian.hamilton.it@gmail.com>
2017-05-17 08:50:45 -0700

    added vapor address model

---
+++ model/address.model.js
@@ -0,0 +1,9 @@
+
+function Address {
+	this.addressLine1;
+	this.addressLine2;
+	this.city,
+	this.state,
+	this.county,
+	this.country
+}
--- router/api-router.js
+++ router/api-router.js
@@ -4,20 +4,29 @@ var wsRouter = express.Router();
 wsRouter.route('/address/id/:id')
     .get(function (req, res) {
         res.status(501).send("get not implemented");   
-    }.post(function (req, res) {
+    }).post(function (req, res) {
          res.status(501).send("post not implemented");
-    }.put(function (req, res) {
+    }).put(function (req, res) {
          res.status(501).send("put not implemented");
-    }.delete(function (req, res) {
+    }).delete(function (req, res) {
         res.status(501).send("delete not implemented");
 });
 
 wsRouter.route('/address/list')
     .get(function (req, res) {
-        Profile.find(function (err, episodes){
-          if(err) res.status(500).send('ERR->' + err); else res.json(episodes);  
-        });    
+        res.json([
+                    {
+                      "addressLine1": "690 Regal Lane",
+                      "addressLine2": "Back yard",
+                      "city": "Aurora",
+                      "state": "IL",
+                      "county": "Kane",
+                      "country": "United States"
+                    }              
+                  ]
+                  );  
+    
     });
-});
+
 
 module.exports = wsRouter;
